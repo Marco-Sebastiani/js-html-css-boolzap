818e5d61c8ab62aa3f48451b4eb9add71be1009f
Marco.seb <marco.seba96@gmail.com>
2021-02-02 12:41:49 +0100

    upgrade

--- vue/script.js
+++ vue/script.js
@@ -4,39 +4,48 @@ var app = new Vue({
         contacts:[
             {
                 name:'Michele',
-                avatar:'img/avatar_1.jpg'
+                avatar:'img/avatar_1.jpg',
+                title: 'avatar_1'
             },
             {
                 name:'Fabio',
-                avatar:'img/avatar_3.jpg'
+                avatar:'img/avatar_3.jpg',
+                title: 'avatar_3'
             },
             {
                 name:'Mario',
-                avatar:'img/avatar_2.jpg'
+                avatar:'img/avatar_2.jpg',
+                title: 'avatar_2'
             },
             {
                 name:'Samuele',
-                avatar:'img/avatar_4.jpg'
+                avatar:'img/avatar_4.jpg',
+                title: 'avatar_4'
             },
             {
                 name:'Giulia',
-                avatar:'img/avatar_io.jpg'
+                avatar:'img/avatar_io.jpg',
+                title: 'avatar_io'
             },
             {
                 name:'Lorenzo',
-                avatar:'img/avatar_5.jpg'
+                avatar:'img/avatar_5.jpg',
+                title: 'avatar_5'
             },
             {
                 name:'Luca',
-                avatar:'img/avatar_7.jpg'
+                avatar:'img/avatar_7.jpg',
+                title: 'avatar_7'
             },
             {
                 name:'Giovanni',
-                avatar:'img/avatar_8.jpg'
+                avatar:'img/avatar_8.jpg',
+                title: 'avatar_8'
             },
             {
                 name:'Marco Sebastiani',
-                avatar:'img/avatar_3.jpg'
+                avatar:'img/avatar_3.jpg',
+                title: 'account-user'
             }
 
         ]
