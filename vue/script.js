a43db7ea793e998d60a97ebf876c01d8ef6a113a
Marco.seb <marco.seba96@gmail.com>
2021-02-02 11:18:12 +0100

    add js

---
+++ vue/script.js
@@ -0,0 +1,24 @@
+var app = new Vue({
+    el:'app',
+    data:{
+        contacts:[
+            {
+                name:'Michele',
+                avatar:'../img/avatar_1.jpg',
+            },
+            {
+                name:'Fabio',
+                avatar:'../img/avatar_3.jpg',
+            },
+            {
+                name:'Mario',
+                avatar:'../img/avatar_2.jpg',
+            },
+            {
+                name:'Samuele',
+                avatar:'../img/avatar_4.jpg',
+            },
+
+        ]
+    }
+});
